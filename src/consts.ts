55262c24ad297777705fe0f65e09e8a518565df7
Charlotte-Greenaway <charlottegreenaway654@gmail.com>
2026-04-11 23:29:15 +0100

    more seo stuff reg image loading

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Allotment Blog UK | Charlotte’s Gardening & Allotment Diary';
-export const SITE_DESCRIPTION = 'Welcome to my allotment website! Here, I share my journey of cultivating a thriving garden, filled with tips, insights, and stories about my experiences with growing plants and nurturing nature.';
+export const SITE_DESCRIPTION = 'Allotment gardening blog from the UK. Charlotte shares tips, mistakes, and experiences growing vegetables, fruit trees, and seasonal crops.';
 export const BLOG_POSTS_PER_PAGE = 5;
